275d8d3c48a07052148292e76d85fe5aeaa12bb9
Harrison Conlin <me@harrisony.com>
2015-06-24 15:31:48 +1000

    It's a start

---
+++ scratchpad.js
@@ -0,0 +1,18 @@
+var injector = document.createElement("script");injector.src = "//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js";injector.type="text/javascript";document.getElementsByTagName("head")[0].appendChild(injector);
+// stupid blackboard
+var $j = jQuery.noConflict();
+
+var questions = $j('.contentListPlain');
+$j(questions).children().each(function(){
+  $j(this).find('.contentListRight').remove();
+  $j(this).find('.reviewTestSubCellForIconBig').empty();
+  $j(this).find('.correctAnswerFlag').remove();
+  $j(this).find('.details td[valign=top] tr:contains("Selected Answer:")').remove();
+  $j(this).find('.details td[valign=top] tr:contains("Answer Feedback:")').remove();
+  $j(this).find('.details td[valign=top] tr:contains("Response Feedback:")').remove();
+  $j(this).find('.details td[valign=top] li.matchingAnswers td[headers!=question]').remove();
+  $j(this).find('.details td[valign=top] .reviewQuestionsAnswerDiv .answerTextSpan pre').text('free text entry');
+  $j(this).find('.details td[valign=top] .reviewQuestionsAnswerDiv').each(function(i) {
+    $j(this).parent().siblings().text(String.fromCharCode('A'.charCodeAt() + i) + '.');
+  });
+});
